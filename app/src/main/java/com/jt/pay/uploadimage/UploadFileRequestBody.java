9ff70657f3e032690d2b95865a0fc73974ad73e8
ThreeEyedCrow <noah9456875100@gmail.com>
2018-05-23 11:59:31 +0800

    设置传输流大小1M

--- app/src/main/java/com/jt/pay/uploadimage/UploadFileRequestBody.java
+++ app/src/main/java/com/jt/pay/uploadimage/UploadFileRequestBody.java
@@ -1,7 +1,11 @@
 package com.jt.pay.uploadimage;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
@@ -18,10 +22,12 @@ public class UploadFileRequestBody extends RequestBody {
 
     private RequestBody mRequestBody;
     private FileUploadObserver<ResponseBody> fileUploadObserver;
+    private File file;
 
     public UploadFileRequestBody(File file, FileUploadObserver<ResponseBody> fileUploadObserver) {
         this.mRequestBody = RequestBody.create(MediaType.parse("application/octet-stream"), file);
         this.fileUploadObserver = fileUploadObserver;
+        this.file = file;
     }
 
     @Override
@@ -36,14 +42,21 @@ public class UploadFileRequestBody extends RequestBody {
 
     @Override
     public void writeTo(BufferedSink sink) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        int length;
+        byte[] buffer = new byte[1024 * 1024];
+        while ((length = fileInputStream.read(buffer)) != -1) {
+            sink.write(buffer, 0, length);
+        }
 
-        CountingSink countingSink = new CountingSink(sink);
-        BufferedSink bufferedSink = Okio.buffer(countingSink);
-        //写入
-        mRequestBody.writeTo(bufferedSink);
-        //刷新
-        //必须调用flush，否则最后一部分数据可能不会被写入
-        bufferedSink.flush();
+
+//        CountingSink countingSink = new CountingSink(sink);
+//        BufferedSink bufferedSink = Okio.buffer(countingSink);
+//        //写入
+//        mRequestBody.writeTo(bufferedSink);
+//        //刷新
+//        //必须调用flush，否则最后一部分数据可能不会被写入
+//        bufferedSink.flush();
     }
 
     protected final class CountingSink extends ForwardingSink {
